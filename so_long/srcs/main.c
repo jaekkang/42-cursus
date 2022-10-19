50c18c1414bd30b9099f533de4f4b19695ec2f33
JaeKwon Kang <45617104+jaekwonsHub@users.noreply.github.com>
2022-10-19 18:27:38 +0900

    rm ojects

--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jaekkang <jaekkang@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 14:50:28 by jaekkang          #+#    #+#             */
-/*   Updated: 2022/10/19 18:26:40 by jaekkang         ###   ########.fr       */
+/*   Updated: 2022/10/19 18:27:33 by jaekkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	so_long(t_game *game)
 	mlx_hook(game->win, 2, 0, &key_press, game);
 	mlx_hook(game->win, X_EVENT_KEY_EXIT, 0, &exit_game, game);
 	mlx_loop(game->mlx);
-}`
+}
 
 int	main(int ac, char **av)
 {
