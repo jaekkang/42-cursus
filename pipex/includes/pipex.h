e7fa08f26a595bf41862bf7ef3a2edc5680287f5
JaeKwon Kang <45617104+jaekwonsHub@users.noreply.github.com>
2022-11-03 23:51:18 +0900

    pipe

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,20 +6,42 @@
 /*   By: jaekkang <jaekkang@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:15:35 by jaekkang          #+#    #+#             */
-/*   Updated: 2022/11/02 16:00:54 by jaekkang         ###   ########.fr       */
+/*   Updated: 2022/11/03 23:02:01 by jaekkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/includes/libft.h"
-# include <sys/wait.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <pthread.h>
+# include "../libft/includes/libft.h" // need utils
+# include <sys/wait.h> // wait && waitpid
+# include <unistd.h> // close , read -
+# include <stdlib.h> // malloc
+# include <stdio.h> // perror
+# include <errno.h> 
+# include <fcntl.h> // open()
 # include <string.h>
-# include <errno.h>
+
+# define BUFSIZE 1024
+
+typedef struct s_data
+{
+	int		fd[2];
+	char	**argv;
+	char	**cmd1;
+	char	**cmd2;
+	char	*tmp_path;
+	char	**paths;
+	int		in_fd;
+	int		out_fd;
+	char	**ep;
+	char	*cmd1_path;
+	char	*cmd2_path;
+	char	buffer[BUFSIZE];
+	pid_t	pid;
+}			t_data;
+
+void	find_path(t_data *data);
+void	parse_data(t_data *data, int ac, char **av, char **envp);
 
 #endif
--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,47 +6,85 @@
 /*   By: jaekkang <jaekkang@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:51:39 by jaekkang          #+#    #+#             */
-/*   Updated: 2022/11/03 15:33:37 by jaekkang         ###   ########.fr       */
+/*   Updated: 2022/11/03 23:03:40 by jaekkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-#include <fcntl.h>
 
-static char	*find_path(char **envp)
+void	work_parent_process(t_data *data)
 {
-	while (ft_strncmp("PATH", *envp, 4))
-		envp++;
-	return (*envp + 5);
+	int	i;
+
+	i = 0;
+	while (data->paths[i] != NULL)
+	{
+		data->cmd2_path = ft_strjoin(data->paths[i], data->cmd2[0]);
+		if (access(data->cmd2_path, X_OK) == 0)
+			break ;
+		i++;
+		if (data->paths[i] == NULL)
+			perror("Error\ncommand not found");
+	}
+	dup2(data->fd[0], STDIN_FILENO);
+	dup2(data->out_fd, STDOUT_FILENO);
+	close(data->fd[1]);
+	if (execve(data->cmd2_path, data->cmd2, data->ep) == -1)
+	{
+		perror("Error\n");
+		exit(1);
+	}
+}
+
+void	work_child_process(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->paths[i] != NULL)
+	{
+		data->cmd1_path = ft_strjoin(data->paths[i], data->cmd1[0]);
+		if (access(data->cmd1_path, X_OK) == 0)
+			break ;
+		i++;
+		if (data->paths[i] == NULL)
+			perror("Error\ncommand not found");
+	}
+	dup2(data->in_fd, STDIN_FILENO);
+	dup2(data->fd[1], STDOUT_FILENO);
+	close(data->fd[0]);
+	if (execve(data->cmd1_path, data->cmd1, data->ep) == -1)
+	{
+		perror("Error\n");
+		exit(1);
+	}
 }
 
 int	main(int ac, char **av, char **envp)
 {
-	int	fds[2];
-	int	child_num = 0;
-	pid_t pid = fork();
-	printf("access return : %d\n", access("infile", X_OK | R_OK ));
-	pipe(fds);
-	if (pid != 0)
+	t_data	data;
+
+	if (ac != 5)
 	{
-		child_num++;
-		pid = fork();
-		close(fds[0]);
-		close(fds[1]);
+		perror("Error\nneed 5 params only\n");
+		exit(1);
 	}
+	parse_data(&data, ac, av, envp);
+	find_path(&data);
+	if (pipe(data.fd) == -1)
+	{
+		perror("Error\n-> in pipe\n");
+		exit(1);
+	}
+	data.pid = fork();
+	if (data.pid == -1)
+	{
+		perror("Error\n-> in fork()\n");
+		exit(1);
+	}
+	if (data.pid == 0)
+		work_child_process(&data);
 	else
-	{
-		if (child_num == 0)
-		{
-			execvp("ls", &av[1]);
-			close(fds[0]);
-		}
-		else if (child_num == 1)
-		{
-			printf("second\n");
-			close(fds[1]);
-		}
-		printf("hello\n");
-	}
+		work_parent_process(&data);
 	return (0);
 }
