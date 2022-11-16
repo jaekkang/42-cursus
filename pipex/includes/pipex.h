9ef68694a15e3eb929f8c4721546bc967572b6e3
JaeKwon Kang <45617104+jaekwonsHub@users.noreply.github.com>
2022-11-16 19:20:15 +0900

    .

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,18 +6,18 @@
 /*   By: jaekkang <jaekkang@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:15:35 by jaekkang          #+#    #+#             */
-/*   Updated: 2022/11/16 16:46:29 by jaekkang         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:18:35 by jaekkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/includes/libft.h" // utils
-# include <unistd.h> // close , read
-# include <stdlib.h> // malloc
-# include <stdio.h> // perror
-# include <fcntl.h> // open()
+# include "../libft/includes/libft.h"	 // utils
+# include <unistd.h>			    	 // close() , read()
+# include <stdlib.h> 					 // malloc()
+# include <stdio.h>						 // perror()
+# include <fcntl.h> 					 // open()
 
 typedef enum errcode {
 	ARGC = 0,
