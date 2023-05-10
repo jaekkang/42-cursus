37a143e8c462fd2e1f5bdb8de6042fd3114336f2
JaeKwon Kang <45617104+jaekkang@users.noreply.github.com>
2023-05-10 10:34:22 +0900

    check CPP03 ex02

---
+++ CPP_Module/CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class FragTrap : public ClapTrap {
+  private:
+  public:
+    FragTrap();
+    FragTrap(std::string name);
+    ~FragTrap();
+    FragTrap(const FragTrap &obj);
+    FragTrap &operator=(const FragTrap &obj);
+
+    void highFivesGuys(void);
+};
+
+#endif
---
+++ CPP_Module/CPP03/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include "FragTrap.hpp"
+
+int main(void) {
+    FragTrap new1;
+
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.attack("checkName");
+    new1.takeDamage(30);
+    new1.takeDamage(30);
+    new1.takeDamage(30);
+    new1.takeDamage(30);
+
+    new1.highFivesGuys();
+}
