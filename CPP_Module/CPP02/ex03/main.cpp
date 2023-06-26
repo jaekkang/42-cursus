93a94ae44eb11cb8d386d0640492202ce50fbc88
jaekkang <rkdwornjs123@gmail.com>
2023-06-26 13:23:59 +0900

    ex03

--- CPP_Module/CPP02/ex03/main.cpp
+++ CPP_Module/CPP02/ex03/main.cpp
@@ -5,7 +5,8 @@ int main(void) {
   Point a(Fixed(1), Fixed(1));
   Point b(Fixed(1), Fixed(5));
   Point c(Fixed(5), Fixed(1));
-  Point p(Fixed(2.1), Fixed(2.1));
+
+  Point p(Fixed(1), Fixed(1));
 
   std::cout << std::boolalpha << bsp(a, b, c, p) << std::endl;
   return 0;
