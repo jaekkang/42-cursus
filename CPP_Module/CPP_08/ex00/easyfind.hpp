c3378813f29a3f096014aec774a28e5b06b0701c
JaeKwon Kang <45617104+jaekkang@users.noreply.github.com>
2023-09-05 10:50:09 +0900

    ex00 finish

--- CPP_Module/CPP_08/ex00/easyfind.hpp
+++ CPP_Module/CPP_08/ex00/easyfind.hpp
@@ -1,12 +1,21 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 template <typename T>
 
-T easyfind(T a, int b) {
-	
+int easyfind(T a, int b) {
+    typename T::iterator it;
+
+    it = std::find(a.begin(), a.end(), b);
+    if (it != a.end()) {
+        std::cout << *it << std::endl;
+        return *it;
+    } else
+        throw std::exception();
 }
 
 #endif
