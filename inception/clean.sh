abbba0589963a008cdb082d3c65cc523396afc23
JaeKwon Kang <45617104+jaekkang@users.noreply.github.com>
2024-01-23 16:07:31 +0900

    add inception

--- inception/clean.sh
+++ inception/clean.sh
@@ -5,7 +5,7 @@ docker rm $(docker ps -qa) 2>/dev/null
 docker rmi -f $(docker images -qa) 2>/dev/null
 docker volume rm $(docker volume ls -q) 2>/dev/null
 docker network rm $(docker network ls -q) 2>/dev/null
-rm -rf /home/juyojeon/data/mysql/*
-rm -rf /home/juyojeon/data/www/*
+rm -rf /home/jaekkang/data/mysql/*
+rm -rf /home/jaekkang/data/www/*
 
 exit 0
--- inception/srcs/requirements/nginx/tools/configuration.sh
+++ inception/srcs/requirements/nginx/tools/configuration.sh
@@ -3,9 +3,9 @@
 if [ ! -d "/etc/nginx/ssl" ]; then
        mkdir /etc/nginx/ssl
        openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
-              -out /etc/nginx/ssl/juyojeon.crt \
-              -keyout /etc/nginx/ssl/juyojeon.key \
-              -subj "/C=KR/ST=Seoul/L=Seoul/O=42 School/OU=student/CN=juyojeon/"
+              -out /etc/nginx/ssl/jaekkang.crt \
+              -keyout /etc/nginx/ssl/jaekkang.key \
+              -subj "/C=KR/ST=Seoul/L=Seoul/O=42 School/OU=student/CN=jaekkang/"
        mkdir -p /run/nginx
 fi
 
